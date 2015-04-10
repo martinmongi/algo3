465a70b9e0efc5f34d5758d8a91a2154c1094d81
Martín Mongi Badía <martinmongi@gmail.com>
2015-04-10 14:56:08 -0300

    Iteracion de movimientos de caballo en el ejercicio 3

--- tp1/3/problema_3.cpp
+++ tp1/3/problema_3.cpp
@@ -9,11 +9,16 @@ using namespace std;
 #define INCR_THREAT 1
 #define TAB '\t'
 
+int upper_bound;
+vector<pair<int, int> > offsets = {{0,0},{2,1},{1,2},{1,-2},{2,-1},{-1,2},{-2,1},{-1,-2},{-2,-1}};
+
 int main(){
+	
 	int n, k, i, x, y;
 
 	cin >> n >> k;
 
+	upper_bound = n*n;
 	vector<vector<char> > board(n, vector<char>(n, 0)); //Tablero en cero todo
 
 	for(i = 0; i < k; i++){
@@ -98,114 +103,39 @@ pair<int, int> find_blank(vector<vector<char> > &board){
 
 vector<vector<char> > solve(vector<vector<char> > board){
 	int n = board.size();
-	int min_horses, horses;
+	int min_horses = n*n, horses;
 
 	pair<int, int> first_blank = find_blank(board);
 
 	//Si llego al final del tablero sin encontrar 0 significa que esta todo cubierto, entonces, devuelvo el que me pasan
-	if(first_blank.first == n)
+	if(first_blank.first == n){
+		horses = count_new_horses(board);
+		if(horses < upper_bound)
+			upper_bound = horses;
 		return board;
+	}
 
 	//en i, j esta el primer casillero no expuesto
 
 	int x = first_blank.first, y = first_blank.second;
 	vector<vector<char> > sug_board, solution;
+	int horse_x, horse_y;
 
-	//case 1
-	sug_board = board;
-	add_horse(sug_board, x, y, NEW_HORSE);
-	sug_board = solve(sug_board);
-	horses = count_new_horses(sug_board);
-	min_horses = horses;
-	solution = sug_board;
 	
-	//case 2
-	sug_board = board;
-	if(x-1 >= 0 && y-2 >= 0){
-		add_horse(sug_board, x-1, y-2, NEW_HORSE);
-		sug_board = solve(sug_board);
-		horses = count_new_horses(sug_board);
-		if(horses < min_horses){
-			min_horses = horses;
-			solution = sug_board;
+	for(unsigned int i = 0; i < offsets.size(); i++){
+		horse_x = x + offsets[i].first;
+		horse_y = y + offsets[i].second;
+		if(horse_x >= 0 && horse_x < n && horse_y >= 0 && horse_y < n){
+			sug_board = board;	
+			add_horse(sug_board, horse_x, horse_y, NEW_HORSE);
+			sug_board = solve(sug_board);
+			horses = count_new_horses(sug_board);
+			if(horses < min_horses){
+				min_horses = horses;
+				solution = sug_board;
+			}
 		}
 	}
-	//case 3
-	sug_board = board;
-	if(x-2 >= 0 && y-1 >= 0){
-		add_horse(sug_board, x-2, y-1, NEW_HORSE);
-		sug_board = solve(sug_board);
-		horses = count_new_horses(sug_board);
-		if(horses < min_horses){
-			min_horses = horses;
-			solution = sug_board;
-		}
-	}
-	//case 4
-	sug_board = board;
-	if(x+1 < n && y-2 >= 0){
-		add_horse(sug_board, x+1, y-2, NEW_HORSE);
-		sug_board = solve(sug_board);
-		horses = count_new_horses(sug_board);
-		if(horses < min_horses){
-			min_horses = horses;
-			solution = sug_board;
-		}
-	}
-	//case 5
-	sug_board = board;
-	if(x+2 < n && y-1 >= 0){
-		add_horse(sug_board, x+2, y-1, NEW_HORSE);
-		sug_board = solve(sug_board);
-		horses = count_new_horses(sug_board);
-		if(horses < min_horses){
-			min_horses = horses;
-			solution = sug_board;
-		}
-	}
-	//case 6
-	sug_board = board;
-	if(x+1 < n && y+2 < n){
-		add_horse(sug_board, x+1, y+2, NEW_HORSE);
-		sug_board = solve(sug_board);
-		horses = count_new_horses(sug_board);
-		if(horses < min_horses){
-			min_horses = horses;
-			solution = sug_board;
-		}
-	}
-	//case 7
-	sug_board = board;
-	if(x+2 < n && y+1 < n){
-		add_horse(sug_board, x+2, y+1, NEW_HORSE);
-		sug_board = solve(sug_board);
-		horses = count_new_horses(sug_board);
-		if(horses < min_horses){
-			min_horses = horses;
-			solution = sug_board;
-		}
-	}
-	//case 8
-	sug_board = board;
-	if(x-1 >= 0 && y+2 < n){
-		add_horse(sug_board, x-1, y+2, NEW_HORSE);
-		sug_board = solve(sug_board);
-		horses = count_new_horses(sug_board);
-		if(horses < min_horses){
-			min_horses = horses;
-			solution = sug_board;
-		}
-	}
-	//case 9
-	sug_board = board;
-	if(x-2 >= 0 && y+1 < n){
-		add_horse(sug_board, x-2, y+1, NEW_HORSE);
-		sug_board = solve(sug_board);
-		horses = count_new_horses(sug_board);
-		if(horses < min_horses){
-			min_horses = horses;
-			solution = sug_board;
-		}
-	}
+
 	return solution;
 }
